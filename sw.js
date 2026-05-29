5964d8bafeac4c1622f241ec30051a54e131ae20
matharashfaq-prog <matharashfaq@gmail.com>
2026-05-29 23:16:11 +0500

    Add service worker for caching and offline support

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'athar-food-v2.5';
+const ASSETS = [
+  './',
+  './index.html'
+];
+
+// انسٹالیشن اور فائلیں سیو کرنا
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+// پرانا کیشے صاف کرنا اور نئی اپڈیٹ لانا
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// آف لائن موڈ میں فائلوں کو موبائل سے ہی چلانا
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request).catch(() => caches.match('./index.html'));
+    })
+  );
+});
